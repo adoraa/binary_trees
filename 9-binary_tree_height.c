f83d585b501e4505737285edd1367e4b30fdb84f
Adora <adoraonyile@gmail.com>
2024-04-11 17:42:14 +0100

    measures the size of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,16 +1,4 @@
 #include "binary_trees.h"
-/**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
- *
- * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node != NULL && node->left == NULL && node->right == NULL)
-		return (1);
-	return (0);
-}
 
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -20,7 +8,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 
 	size_t height_left = binary_tree_height(tree->left);
