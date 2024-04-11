482711a9121b84de4143a4d980504716f1e6b090
Adora <adoraonyile@gmail.com>
2024-04-11 17:51:13 +0100

    measures the size of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,19 +3,21 @@
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
+ *
  * Return: Height of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left, height_right;
 
 	if (tree == NULL)
 		return (0);
 
-	size_t height_left = binary_tree_height(tree->left);
-	size_t height_right = binary_tree_height(tree->right);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
-	if (height_left >= height_right)
-		return (1 + height_left);
+	if (height_left > height_right)
+		return (height_left + 1);
 	else
-		return (1 + height_right);
+		return (height_right + 1);
 }
