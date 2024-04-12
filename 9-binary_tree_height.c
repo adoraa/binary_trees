7eaa1b9698864ee4ba3cce5e92b66f3137ac398c
Adora <adoraonyile@gmail.com>
2024-04-12 03:57:41 +0100

    checker fail fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,18 +3,15 @@
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
- *
  * Return: Height of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
-
 	if (tree == NULL)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
+	size_t height_left = binary_tree_height(tree->left);
+	size_t height_right = binary_tree_height(tree->right);
 
 	if (height_left > height_right)
 		return (height_left + 1);
