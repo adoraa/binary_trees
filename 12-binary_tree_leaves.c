727b5bb104acd6a432368b6ad67a9aad5afcd451
Adora <adoraonyile@gmail.com>
2024-04-12 04:17:26 +0100

    checker fail fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves_left = 0, leaves_right = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	size_t leaves_left = binary_tree_leaves(tree->left);
-	size_t leaves_right = binary_tree_leaves(tree->right);
+	leaves_left = binary_tree_leaves(tree->left);
+	leaves_right = binary_tree_leaves(tree->right);
 
 	return (leaves_left + leaves_right);
 }
