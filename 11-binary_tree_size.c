6a4ed38674262aea3ebb167276e8fd5a1e31c22e
Adora <adoraonyile@gmail.com>
2024-04-12 04:14:37 +0100

    checker error

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,8 +10,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
+	size_t left_size = binary_tree_size(tree->left) + 1;
+	size_t right_size = binary_tree_size(tree->right) + 1;
 
-	return (1 + left_size + right_size);
+	return ((left_size + right_size) - 1);
 }
